6e097e852613986e6b0ec6de2311ddc87872ddc3
Manni Moghimi <mannimoghimi@gmail.com>
2023-10-09 22:53:38 +0330

    feat: integrated post app and models with s3 for storing images

--- api/post/app.py
+++ api/post/app.py
@@ -1,9 +1,12 @@
-import json
 import base64
+import json
+import os
+import uuid
 
 from config import Config
 from api.post import db
 from api.post.models import UserData
+from s3.client import S3Client
 
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
@@ -14,6 +17,8 @@ app.config.from_object(Config)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+s3 = S3Client()
 
 @app.route("/api", methods=["POST"])
 def request_api():
@@ -26,16 +31,23 @@ def request_api():
     name = request_json["name"]
     national_id = request_json["national_id"]
 
-    photo1 = request.files['photo1'].read()
-    photo2 = request.files['photo2'].read()
+    photo1 = request.files['photo1']
+    photo2 = request.files['photo2']
+
+    # TODO: refactor storing photos on s3
+    _, photo1_ext = os.path.splitext(photo1.filename)
+    _, photo2_ext = os.path.splitext(photo2.filename)
+
+    photo1_s3_url = s3.put_object(photo1, f"{uuid.uuid4()}{photo1_ext}")
+    photo2_s3_url = s3.put_object(photo2, f"{uuid.uuid4()}{photo2_ext}")
 
     user_data = UserData(
         ip_address=ip_address,
         email=email,
         name=name,
         national_id=national_id,
-        photo1=photo1,
-        photo2=photo2,
+        photo1_s3_url=photo1_s3_url,
+        photo2_s3_url=photo2_s3_url,
     )
 
     db.session.add(user_data)
--- api/post/models.py
+++ api/post/models.py
@@ -3,9 +3,9 @@ from api.post import db
 
 class UserData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     name = db.Column(db.String(255), nullable=False)
-    national_id = db.Column(db.String(10), unique=True, nullable=False)
+    national_id = db.Column(db.String(10), nullable=False, unique=True)
     ip_address = db.Column(db.String(15), nullable=False)
-    photo1 = db.Column(db.LargeBinary, nullable=False)
-    photo2 = db.Column(db.LargeBinary, nullable=False)
+    photo1_s3_url = db.Column(db.String(255), nullable=False, unique=True)
+    photo2_s3_url = db.Column(db.String(255), nullable=False, unique=True)
