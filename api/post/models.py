e4b88f3a8ce30153802fa7a9976cab0faa7eab96
Manni Moghimi <mannimoghimi@gmail.com>
2023-10-10 22:54:04 +0330

    feat: added status field to user_data model

--- api/post/models.py
+++ api/post/models.py
@@ -1,5 +1,9 @@
 from api.post import db
 
+from sqlalchemy.dialects import postgresql
+
+
+StatusEnum = postgresql.ENUM("pending", "accepted", "rejected", name="status_enum", create_type=True)
 
 class UserData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,3 +14,4 @@ class UserData(db.Model):
     ip_address = db.Column(db.String(15), nullable=False)
     photo1_s3_url = db.Column(db.String(255), nullable=False, unique=True)
     photo2_s3_url = db.Column(db.String(255), nullable=False, unique=True)
+    status = db.Column(StatusEnum, default="pending", nullable=False)
