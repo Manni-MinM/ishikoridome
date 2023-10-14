2fc942148d5c6611d88b87803f79f9d6c2b62382
Manni Moghimi <mannimoghimi@gmail.com>
2023-10-15 00:53:39 +0330

    feat: integrated status check app with image api client

--- apps/status/__init__.py
+++ apps/status/__init__.py
@@ -1,3 +1,4 @@
+from apps.status.image_api.client import ImageAPIClient
 from rabbitmq.client import RabbitMQClient
 from s3.client import S3Client
 
@@ -7,3 +8,4 @@ from flask_sqlalchemy import SQLAlchemy
 s3 = S3Client()
 db = SQLAlchemy()
 rbmq = RabbitMQClient()
+imagga = ImageAPIClient()
--- apps/status/app.py
+++ apps/status/app.py
@@ -1,4 +1,6 @@
-from apps.status import db, rbmq
+from apps.post import models
+from apps.status import db, s3, rbmq, imagga
+from apps.status.image_api import client
 from config import Config
 
 from flask import Flask
@@ -9,10 +11,34 @@ app.config.from_object(Config)
 
 db.init_app(app)
 
-# FIXME: test function for now
-def log(national_id):
-    print(f"Received message with national_id: {national_id}")
+def face_tagged(photo_s3_url, threshold):
+    photo = s3.get_object(photo_s3_url)
+
+    try:
+        res = imagga.face_detection(photo, threshold)
+        return True
+
+    except client.TaggingException as err:
+        return False
+
+def process(national_id):
+    query = models.UserData.query.with_session(db.session)
+    user_data = query.filter_by(national_id=national_id).first()
+
+    confidence_threshold = Config.IMAGGA_TAGGER_FACE_CONFIDENCE_THRESHOLD
+
+    try:
+        photo1_has_face = face_tagged(user_data.photo1_s3_url, confidence_threshold)
+        photo2_has_face = face_tagged(user_data.photo2_s3_url, confidence_threshold) 
+
+        if photo1_has_face and photo2_has_face:
+            print(f"[SUCCESS] images of user with email: {user_data.email} are valid.")
+        else:
+            print(f"[FAIL] images of user with email: {user_data.email} are invalid.")
+
+    except Exception as err:
+        print(f"[ERROR] request failed due to following error: {err}")
 
 with app.app_context():
     # TODO: error handling for connection to rbmq
-    rbmq.process_messages(log)
+    rbmq.process_messages(process)
